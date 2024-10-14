ff8c9fbeb684c818cf94e48cc2ebddc70a1db5af
cybershadow786 <imadeelwaheed@gmail.com>
2024-10-15 03:20:46 +0500

    All Done

--- src/Components/Utilites/Navbar/Navbar.jsx
+++ src/Components/Utilites/Navbar/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="bg-white sticky dark:bg-gray-900 w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+      <nav className="sticky bg-gray-900 w-full z-20 top-0 start-0 border-b border-gray-200 border-gray-600">
         <div className="max-w-screen-xl _910:max-w-screen-xl _910:mx-auto flex flex-wrap items-center justify-between mx-auto p-4">
           <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white text-center _768:w-[100vw] _768:mb-4 _890:mb-0 _890:w-auto">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white text-center _768:w-[100vw] _768:mb-4 _890:mb-0 _890:w-auto">
               AZ_Shoe_Maker
             </span>
           </Link>
@@ -17,7 +17,7 @@ const Navbar = () => {
             <button
               onClick={() => setIsMenuOpen(!isMenuOpen)}
               type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
               aria-controls="navbar-sticky"
               aria-expanded={isMenuOpen}
             >
@@ -39,7 +39,7 @@ const Navbar = () => {
             }`}
             id="navbar-sticky"
           >
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white bg-gray-800 md:bg-gray-900 border-gray-700">
               {[
                 { name: "Home", path: "/" },
                 { name: "About", path: "/about" },
